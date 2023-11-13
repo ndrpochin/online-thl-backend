cca643dbdccdcbbdd58b2c764de4fa527a11e82b
Nadir Eliel Jaramillo Rosas <nadirjaramillorosas@hotmail.com>
2023-11-13 19:51:13 +1300

    update cors policy

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://online-thl.vercel.app/']
+  origin: ['http://localhost:3000', 'https://online-thl.vercel.app']
 }));
 
 const PORT = 3000
