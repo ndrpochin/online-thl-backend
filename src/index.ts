299aa0b9fa8be685311332025571df9f12496a98
Nadir Eliel Jaramillo Rosas <nadirjaramillorosas@hotmail.com>
2024-02-12 16:05:09 +1300

    Se quita SocketIO

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import express from 'express'
-import { setSocketIO, router as bayRoutes } from './bays/bays';
+import { router as bayRoutes } from './bays/bays';
 import cors from 'cors'
 import http from 'http'
-import { Server } from 'socket.io'
 
 const app = express()
 const server = http.createServer(app)
@@ -12,9 +11,6 @@ app.use(express.json())
 app.use(cors({
   origin: ['http://localhost:3000', 'https://online-thl.vercel.app']
 }));
-const io = new Server(server, {
-  cors: { origin: "http://localhost:3000" },
-})
 
 const PORT = 4000
 
@@ -23,17 +19,10 @@ app.get('/ping', (_req, res) => {
   res.send('pong')
 })
 
-setSocketIO(io)
 app.use('/api/bays', bayRoutes)
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
-io.on("connection", (socket) => {
-  console.log(`a user connected ${socket.id}`);
-  socket.on('put-event', (data) => {
-    socket.broadcast.emit('put-broadcast', data)
-  })
-});
 export default app
